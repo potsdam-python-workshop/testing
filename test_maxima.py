5c1eb159a50ab910d706d5658852ffbe9655f639
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-02-19 11:51:58 +0100

    Some style fixups
    Use pep8 and pyflakes to find issues.

--- test_maxima.py
+++ test_maxima.py
@@ -2,32 +2,24 @@ import numpy as np
 import pytest
 from maxima_good import find_maxima_1
 
-test_cases = [(3,1),(10,1),(7,1)]
+test_cases = [(3, 1),
+              (10, 1),
+              (7, 1)]
 
-@pytest.mark.parametrize('numEl,SeedVal',test_cases)
+@pytest.mark.parametrize('numel,seedval', test_cases)
+def test_randomized(numel, seedval):
+    rand_gen = np.random.RandomState(seed=seedval)
 
-def test_randomized(numEl,SeedVal):
- 
-    RandNumGen = np.random.RandomState(seed=SeedVal)
-
-    test_vec = RandNumGen.random_integers(low=1,high=20,size=numEl)
+    test_vec = rand_gen.random_integers(low=1,high=20,size=numel)
     out      = find_maxima_1(test_vec)
 
-    numMax = len(out);
+    numMax = len(out)
 
     for i in range(0,numMax):
-
-        iE = out[i];
-
+        iE = out[i]
         if iE == 0:
-
             assert test_vec[0] > test_vec[1]
-
-        elif iE == numEl - 1:
-
-            assert test_vec[numEl-1] > test_vec[numEl-2]
-
+        elif iE == numel - 1:
+            assert test_vec[numel-1] > test_vec[numel-2]
         else:
-
             assert test_vec[iE-1] < test_vec[iE] and test_vec[iE+1] < test_vec[iE]
-
