750370699f348302f0dff5d92323b780938fcfdf
Robert Großmann <rgrossmann@uni-potsdam.de>
2019-02-19 11:32:08 +0100

    randomized maximum test with seed control

--- test_maxima.py
+++ test_maxima.py
@@ -2,11 +2,32 @@ import numpy as np
 import pytest
 from maxima_good import find_maxima_1
 
-test_cases = [([0, 1, 2, 1, 2, 1, 0],[2,4]),([-i**2 for i in range(-3, 4)],[3]),([np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)],[16,78]),([1,2,3],[1])]
+test_cases = [(3,1),(10,1),(7,1)]
 
-@pytest.mark.parametrize('inp,exp',test_cases)
+@pytest.mark.parametrize('numEl,SeedVal',test_cases)
 
-def test_maxima(inp,exp):
-    out = find_maxima_1(inp)
-    assert exp == out
+def test_randomized(numEl,SeedVal):
+ 
+    RandNumGen = np.random.RandomState(seed=SeedVal)
+
+    test_vec = RandNumGen.random_integers(low=1,high=20,size=numEl)
+    out      = find_maxima_1(test_vec)
+
+    numMax = len(out);
+
+    for i in range(0,numMax):
+
+        iE = out[i];
+
+        if iE == 0:
+
+            assert test_vec[0] > test_vec[1]
+
+        elif iE == numEl - 1:
+
+            assert test_vec[numEl-1] > test_vec[numEl-2]
+
+        else:
+
+            assert test_vec[iE-1] < test_vec[iE] and test_vec[iE+1] < test_vec[iE]
 
